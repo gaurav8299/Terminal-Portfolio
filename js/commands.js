9158596d61c35b3ab74a4f3eed7dd3acbe5b9b58
Gaurav Rajput <90279818+gaurav8299@users.noreply.github.com>
2024-12-20 14:55:48 +0530

    Initial commit

---
+++ js/commands.js
@@ -0,0 +1,117 @@
+var dev = "https://dev.to/gaurav8299";
+var twitter = "https://x.com/GauravR73998456";
+var linkedin = "https://www.linkedin.com/in/gaurav-rajput01/";
+var instagram = "https://www.instagram.com/r___gaurav";
+var github = "https://github.com/gaurav8299";
+// var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:gauravrajput3005@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Gaurav!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 4th year undergrad, pursuing B.tech.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - UI/UX Designing</pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to work with anyone who wants to build a 
+   professional website for their business or even a personal 
+   website to showcase their portfolio.
+   I love learning new stuff and using it to improve my skills.
+   I primarily use ReactJs and Tailwind CSS when building my 
+   websites (projects).
+   You can learn more about the projects that I have built using 
+   the project command.
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=gauravrajput3005@gmail.com">gauravrajput3005@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'dev        <a href="' + dev + '" target="blank">dev.to/gaurav8299' + "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">x.com/GauravR73998456' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">www.linkedin.com/in/gaurav-rajput01/' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram/' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/gaurav8299' +
+    "</a>",
+
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  `<a href="https://github.com/gaurav8299/Job-Portal-Hired" target="blank"><span class='underline'>Job Portal Hired</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">A functional job portal site named Hired, designed to simplify the job-seeking process. Styled with Tailwind CSS to rapidly build a sleek and user-friendly interface, closely resembling modern job platforms. The site leverages React for a dynamic user experience, Supabase for backend data management and real-time functionality, and Clerk for secure and seamless authentication.
+
+Project is live here <a href="https://job-portal-hired.vercel.app/" target="blank">https://job-portal-hired.vercel.app/</a>
+
+
+Built With
+
+    *React.Js
+    *Tailwind CSS
+    *Supabase
+    *Clerk
+    *NPM
+`,
+
+  "<br>",
+
+  `<a href="https://github.com/gaurav8299/random-geeta-verse-extension" target="blank"><span class='underline'>Random Geeta Verse Chrome Extension</span>`,
+  "<br>",
+  `<pre class="indent-8 whitespace-pre-wrap">The Random Geeta Verse Chrome Extension is a lightweight and simple Chrome extension that provides spiritual inspiration by displaying a random verse from the Bhagavad Gita every time you open it.
+   The extension has a clean and modern user interface with customizable features, making it easy to get a fresh perspective from the Gita with a single click.
+
+</a>
+
+Built With
+
+    *Javascript
+    *CSS
+    *Manifest
+    
+`,
+
+  "<br>",
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
